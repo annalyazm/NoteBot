347ba7f58f2db129f3541795f12a82df25cdf6bf
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-06 08:52:35 +0900

    Update CORE.js

--- CORE.js
+++ CORE.js
@@ -42,7 +42,7 @@ console.log('ohh')
 	const url2 = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 
 
-	let command = msg.content.toLowerCase().split(' ')[1];
+	let command = msg.content.toLowerCase().split(' ')[2];
 	command = command.slice(PREFIX.length)
  
 
