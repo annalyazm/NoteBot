9c402bae52601fa4aa59cce7c844d8cf725e5a77
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-06 09:18:29 +0900

    Update CORE.js

--- CORE.js
+++ CORE.js
@@ -72,7 +72,7 @@ client.on('message', async msg => {
 					let index = 0;
 					msg.channel.send(`
 __**검색결과:**__
-${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length_seconds} 초 )`).join('\n')}
+${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length} )`).join('\n')}
 1 - 5 를 입력하여 선택하시면 됩니다. (20초가 지나면 시간초과로 취소됩니다)
 					`).then((th) => th.delete(20000));
 					try {
@@ -126,7 +126,7 @@ ${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length_secon
 					let index = 0;
 					msg.channel.send(`
 __**검색결과:**__
-${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length_seconds} 초 )`).join('\n')}
+${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length} )`).join('\n')}
 1 - 5 를 입력하여 선택하시면 됩니다. (20초가 지나면 시간초과로 취소됩니다)
 					`).then((th) => th.delete(20000));
 					// eslint-disable-next-line max-depth
@@ -182,13 +182,13 @@ ${videos.map(video2 => `**${++index} -** ${video2.title} ( ${video2.length_secon
 		return msg.channel.send(`볼륨 변경 완료! : **${args[1]}**`);
 	} else if (command === '뭐부르고있음' || command === '지금' || command === '뭐임' || msg.content.startsWith('노트뭐')) {
 		if (!serverQueue) return msg.channel.send('아무것도 안부름');
-		return msg.channel.send(`🎶 지금 부르는거: **${serverQueue.songs[0].title}** ( ${serverQueue.songs[0].title.length_seconds} 초 )`);
+		return msg.channel.send(`🎶 지금 부르는거: **${serverQueue.songs[0].title}** ( ${serverQueue.songs[0].length} )`);
 	} else if (command === '재생목록' || command === '뭐남음' || msg.content.startsWith('노트큐')) {
 		if (!serverQueue) return msg.channel.send('아무것도 안남음');
 		return msg.channel.send(`
 __**재생목록:**__
-${serverQueue.songs.map(song => `**-** ${song.title} ( ${song.length_seconds} 초 ) `).join('\n')}
-**지금 부르는거:** ${serverQueue.songs[0].title} ( ${serverQueue.songs[0].length_seconds} 초 )
+${serverQueue.songs.map(song => `**-** ${song.title} ( ${song.length} ) `).join('\n')}
+**지금 부르는거:** ${serverQueue.songs[0].title} ( ${serverQueue.songs[0].length} )
 		`);
 	} else if (command === '일시정지' || command === '잠만') {
 		if (serverQueue && serverQueue.playing) {
