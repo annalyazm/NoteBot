5c2c60304c5b8d12086294bca41e645cbcf6e4ee
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-06 13:38:49 +0900

    Update CORE.js

--- CORE.js
+++ CORE.js
@@ -200,7 +200,7 @@ ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
 	} else if (msg.content.startsWith('노트볼')) {
 		if (!msg.member.voiceChannel) return msg.channel.send('음성 채널에 먼저 들어와!');
 		if (!serverQueue) return msg.channel.send('부르고 있는 노래가 없어;;');
-		if (!args[1]) return msg.channel.send(`현재 볼륨: **${serverQueue.volume * 100}**`);
+		if (!args[1]) return msg.channel.send(`현재 볼륨: **${serverQueue.volume * 10}**`);
 		serverQueue.volume = args[1];
 		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 100);
 		return msg.channel.send(`볼륨 변경 완료! : **${args[1]}**`);
