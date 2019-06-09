1322c94e70cb22aa586e7ad86f8314c42d71a2c9
SisEver <36586296+rookieman88@users.noreply.github.com>
2019-06-09 20:23:53 +0900

    Update CORE.js

--- CORE.js
+++ CORE.js
@@ -63,7 +63,7 @@ client.on('message', async msg => {
 	if (command === '서버' || msg.content.startsWith('노트섭')) { msg.channel.send(`${client.guilds.size}`) }
         if (command === '공지') {
      let owners = process.env.owners
- let prefix = '노트야 공지 '
+ let prefix = '노트야 공지'
  
     let filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '⭕') && user.id === msg.author.id
     if (owners.includes(msg.author.id)) {
